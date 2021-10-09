37d04a1bc157c34a2e9c5b2442def97bc83688da
Dustin Doctor <dustindoctorz@gmail.com>
2021-10-09 14:53:52 -0400

    gameBoard array now renders to page

--- script.js
+++ script.js
@@ -2,13 +2,26 @@
 
 const gameBoard = (() => {
     const gameBoardArray = new Array(9);
+    
+    const render = () => {
+        let gameTiles = document.getElementsByClassName('tile');
+        console.log(gameTiles);
+        for(let i = 0; i < gameBoardArray.length; i++){
+            gameTiles.item(i).innerHTML = gameBoard.gameBoardArray[i];
+        }
+    }
+    return {
+        gameBoardArray,
+        render
+    }
 })();
 
 const gameController = (() => {
-
+    gameBoard.gameBoardArray = ['X', 'X', 'O', 'X', 'X', 'O', 'X', 'X', 'O'];
+    gameBoard.render(); 
 })();
 
-const player = (symbol) => {
-    
-}
+const player = (symbol, name) => {
 
+    return {symbol, name};
+}
