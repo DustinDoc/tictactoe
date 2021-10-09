52d6ebfa950d495c37e7259aecf701c2bffc8de6
Dustin Doctor <dustindoctorz@gmail.com>
2021-10-09 15:59:47 -0400

    Clicking now places symbols on board

--- script.js
+++ script.js
@@ -2,26 +2,55 @@
 
 const gameBoard = (() => {
     const gameBoardArray = new Array(9);
-    
-    const render = () => {
-        let gameTiles = document.getElementsByClassName('tile');
-        console.log(gameTiles);
-        for(let i = 0; i < gameBoardArray.length; i++){
-            gameTiles.item(i).innerHTML = gameBoard.gameBoardArray[i];
+    let gameTiles = document.getElementsByClassName('tile');
+
+    const initialize = () =>{
+        for(let tile of gameTiles){
+            tile.addEventListener('click', (e) => {
+                placeSymbol(e);
+            })
         }
     }
+
+    const placeSymbol = (e) => {
+        let clickedTile = e.target;
+        if(clickedTile.innerHTML == ""){
+            if(gameController.player1Turn == true){
+                gameBoard.gameBoardArray[clickedTile.dataset.index] = gameController.player1.symbol;
+                clickedTile.innerHTML = gameController.player1.symbol;
+                gameController.player1Turn = false;
+            }else{
+                gameBoard.gameBoardArray[clickedTile.dataset.index] = gameController.player2.symbol;
+                clickedTile.innerHTML = gameController.player2.symbol;
+                gameController.player1Turn = true;
+            }
+        }
+    }
+
     return {
         gameBoardArray,
-        render
+        initialize,
+        placeSymbol
     }
-})();
-
-const gameController = (() => {
-    gameBoard.gameBoardArray = ['X', 'X', 'O', 'X', 'X', 'O', 'X', 'X', 'O'];
-    gameBoard.render(); 
 })();
 
 const player = (symbol, name) => {
 
     return {symbol, name};
 }
+
+const gameController = (() => {
+    var player1Turn = true;
+    gameBoard.initialize();
+
+    const player1 = player('X', 'Player 1');
+    const player2 = player('O', 'Player 2');
+
+    return{
+        player1Turn,
+        player1,
+        player2
+    }
+})();
+
+
