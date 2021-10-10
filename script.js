ca2927b3fc1fec46d85b7546aa8fe286b298271f
Dustin Doctor <dustindoctorz@gmail.com>
2021-10-09 21:10:14 -0400

    Began adding functionality for Starting New Game

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ const gameBoard = (() => {
         let clickedTile = e.target;
         if(clickedTile.innerHTML == ""){
             if(gameController.player1Turn == true){
-                gameBoard.gameBoardArray[clickedTile.dataset.index] = gameController.player1.symbol;
-                clickedTile.innerHTML = gameController.player1.symbol;
-                gameController.player1Turn = false;
+                gameBoard.gameBoardArray[clickedTile.dataset.index] = gameController.getPlayer1Symbol();
+                clickedTile.innerHTML = gameController.getPlayer1Symbol();
+                gameController.changeTurn();
             }else{
-                gameBoard.gameBoardArray[clickedTile.dataset.index] = gameController.player2.symbol;
-                clickedTile.innerHTML = gameController.player2.symbol;
-                gameController.player1Turn = true;
+                gameBoard.gameBoardArray[clickedTile.dataset.index] = gameController.getPlayer2Symbol();
+                clickedTile.innerHTML = gameController.getPlayer2Symbol();
+                gameController.changeTurn();
             }
         }
     }
@@ -34,22 +34,78 @@ const gameBoard = (() => {
     }
 })();
 
-const player = (symbol, name) => {
+const player = (name, symbol, score) => {
 
-    return {symbol, name};
+    return {name, symbol, score};
 }
 
 const gameController = (() => {
     var player1Turn = true;
-    gameBoard.initialize();
+    var player1;
+    var player2;
 
-    const player1 = player('X', 'Player 1');
-    const player2 = player('O', 'Player 2');
+    const getPlayer1Symbol = () => {
+        return player1.symbol;
+    }
+    
+    const getPlayer2Symbol = () => {
+        return player2.symbol;
+    }
+
+    const changeTurn = () => {
+        gameController.player1Turn *= -1;
+    }
+
+    const toggleScoreView = () => {
+        let playerScoreView = document.getElementsByClassName('playerScore');
+        for(let elem of playerScoreView){
+            if(elem.classList.contains('hidden')){
+               elem.classList.remove('hidden');
+            }else{
+                elem.classList.add('hidden');
+            }
+        }
+    }
+
+    const startNewGame = () => {
+        let player1NameField = document.getElementById('player1NameField');
+        let player2NameField = document.getElementById('player2NameField');
+
+        if(player1NameField.textContent == ""){
+            player1 = player('Player 1', 'X', '0');
+        }else{
+            player1 = player(player1NameField.innerText, 'X', '0');
+        }
+
+        if(player2NameField.textContent == ""){
+            player2 = player('Player 2', 'O', '0');
+        }else{
+            player2 = player(player2NameField.innerText, 'O', '0');
+        }
+
+        let player1ScoreHeader = document.getElementById('player1ScoreHeader');
+        let player2ScoreHeader = document.getElementById('player2ScoreHeader');
+
+        player1ScoreHeader.textContent = player1.name;
+        player2ScoreHeader.textContent = player2.name;
+
+        gameBoard.initialize();
+        toggleScoreView();
+    }
+
+    const startGameButton = document.getElementById('nameSubmit');
+    startGameButton.addEventListener('click', () => {
+        startNewGame();
+    })
 
     return{
         player1Turn,
+        changeTurn,
+        toggleScoreView,
         player1,
-        player2
+        player2,
+        getPlayer1Symbol,
+        getPlayer2Symbol
     }
 })();
 
